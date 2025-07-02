57fe5afd0503bf283199938b669c9224d6c4188e
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2025-07-02 20:09:29 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,19 +5,18 @@ firebase.initializeApp({
   apiKey: "AIzaSyCRhjUGAAwWDrxA3AAv5EJuehulBk5hn9E",
   authDomain: "seificontas.firebaseapp.com",
   projectId: "seificontas",
-  storageBucket: "seificontas.firebasestorage.app",
+  storageBucket: "seificontas.appspot.com",
   messagingSenderId: "454487656313",
   appId: "1:454487656313:web:bbc2fe6d220068bdc7d1a8"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/logoseifi192.png'
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+messaging.onBackgroundMessage(payload => {
+  const { title, body } = payload.notification;
+  self.registration.showNotification(title, {
+    body,
+    icon: "/logoseifi192.png"
+  });
 });
+
