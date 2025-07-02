913f7803b06059985d364c1437f262dee581b350
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2025-07-02 19:07:59 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -16,7 +16,7 @@ messaging.onBackgroundMessage(function(payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icons/icon-192.png'
+    icon: '/logoseifi192.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
