9eec7e047274fe1e3411fb0afb2dd816d10d8558
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2025-07-02 12:59:49 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,38 +4,44 @@ const urlsToCache = [
   '/index.html',
   '/app.js',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icons/logoseifi192.png',
+  '/logseifi512.png'
 ];
 
-// Instalação do service worker e cache inicial
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Intercepta requisições e responde com cache se possível
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
     )
   );
 });
 
-// Ativação: limpa caches antigos
-self.addEventListener('activate', event => {
+// Notificação - ao clicar
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+      for (const client of clientList) {
+        if (client.url === '/' && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    })
   );
 });
 
